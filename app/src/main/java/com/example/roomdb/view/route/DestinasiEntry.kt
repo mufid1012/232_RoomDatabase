594dfdea70158872c66521ad415a765c4731440c
mufid1012 <mufidghibran1012@gmail.com>
2025-11-27 17:26:21 +0700

    menambahkan DestinasiEntry

---
+++ app/src/main/java/com/example/roomdb/view/route/DestinasiEntry.kt
@@ -0,0 +1,11 @@
+package com.example.roomdb.view.route
+
+import com.example.roomdb.view.route.DestinasiNavigasi
+import com.example.roomdb.R
+
+object DestinasiEntry : DestinasiNavigasi {
+
+    override val route: String = "item_entry"
+
+    override val titleRes: Int = R.string.entry_siswa
+}
