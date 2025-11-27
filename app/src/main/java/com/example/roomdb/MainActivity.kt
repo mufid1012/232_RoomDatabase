636e697984aedb60ff51242992ba3997c5fa4445
mufid1012 <mufidghibran1012@gmail.com>
2025-11-27 17:28:09 +0700

    feat: menampilkan halaman utama SiswaApp
    Menghapus fungsi Greeting dan GreetingPreview yang sudah tidak digunakan. Menggantinya dengan SiswaApp di dalam Scaffold pada MainActivity.

--- app/src/main/java/com/example/roomdb/MainActivity.kt
+++ app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.roomdb.ui.theme.RoomDBTheme
+import com.example.roomdb.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RoomDBTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SiswaApp(
+                        modifier = Modifier.padding(paddingValues = innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RoomDBTheme {
-        Greeting("Android")
     }
 }
