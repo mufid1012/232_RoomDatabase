22ae954bad614ece4f05bf0fb46e17f948de4fc1
mufid1012 <mufidghibran1012@gmail.com>
2025-11-27 17:28:01 +0700

    menambahkan viewmodel Home

---
+++ app/src/main/java/com/example/roomdb/viewmodel/HomeViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.roomdb.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdb.room.Siswa
+import com.example.roomdb.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> =
+        repositoriSiswa.getAllSiswaStream()       // Flow<List<Siswa>>
+            .map { HomeUiState(listSiswa = it) }  // ubah ke UI state
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = HomeUiState()
+            )
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = emptyList()
+    )
+}
