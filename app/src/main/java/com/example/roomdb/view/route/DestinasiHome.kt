5fa91cb0458f15b56392b6a60e209eb20fda068f
mufid1012 <mufidghibran1012@gmail.com>
2025-11-27 17:26:26 +0700

    menambahkan DestinasiHome

---
+++ app/src/main/java/com/example/roomdb/view/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.roomdb.view.route
+
+import com.example.roomdb.R
+
+object DestinasiHome : DestinasiNavigasi {
+
+    override val route: String = "home"
+
+    override val titleRes: Int = R.string.app_name
+}
