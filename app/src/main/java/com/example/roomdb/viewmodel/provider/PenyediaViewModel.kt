bdc4d88c81fbcfb790d49da586d1a63c810ff5cd
mufid1012 <mufidghibran1012@gmail.com>
2025-12-04 09:05:31 +0700

    menambahkan viewmodel Detail siswa

--- app/src/main/java/com/example/roomdb/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdb/viewmodel/provider/PenyediaViewModel.kt
@@ -5,31 +5,39 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdb.repositori.AplikasiSiswa
-
-    import com.example.roomdb.viewmodel.HomeViewModel
+import com.example.roomdb.viewmodel.DetailViewModel
+import com.example.roomdb.viewmodel.HomeViewModel
 import com.example.roomdb.viewmodel.EntryViewModel
+import androidx.lifecycle.createSavedStateHandle
 
 object PenyediaViewModel {
 
     val Factory = viewModelFactory {
 
+        // Home
         initializer {
             HomeViewModel(
                 repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
             )
         }
 
+        // Entry
         initializer {
             EntryViewModel(
+                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
+        // Detail (PENTING: SavedStateHandle)
+        initializer {
+            val savedStateHandle = this.createSavedStateHandle()
+            DetailViewModel(
+                savedStateHandle = savedStateHandle,
                 repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
             )
         }
     }
 }
 
-/**
- * Fungsi ekstensi CreationExtras
- * Mengambil Application dan mengembalikan instance AplikasiSiswa
- */
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
-    (this[APPLICATION_KEY] as AplikasiSiswa)
+    this[APPLICATION_KEY] as AplikasiSiswa
