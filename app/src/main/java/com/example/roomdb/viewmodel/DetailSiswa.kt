3626ebb49ca08c1cbc79609ce85ad046f0aec419
mufid1012 <mufidghibran1012@gmail.com>
2025-12-04 09:04:54 +0700

    menambahkan DetailSiswa dan fungsi ekstensi toSiswa

---
+++ app/src/main/java/com/example/roomdb/viewmodel/DetailSiswa.kt
@@ -0,0 +1,26 @@
+package com.example.roomdb.viewmodel
+
+import com.example.roomdb.room.Siswa
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa =
+    DetailSiswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
+
+fun DetailSiswa.toSiswa(): Siswa =
+    Siswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
